ca1cd35ca24387093986a6fb2a9baca194c1e959
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-01-25 22:31:04 +0700

    changes environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroModule } from './hero/hero.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from './config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://arizkymuhammad46:C9mlIhgwQUTH2lLa@efektenang.mbnssgs.mongodb.net', { dbName: 'nest-api' }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config]
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configSecret: ConfigService) => ({
+        uri: configSecret.get('mongoUri'),
+      })
+    }),
     HeroModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
+  const port = configService.get('port')
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     }),
   );
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
