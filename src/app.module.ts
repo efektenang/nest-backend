2d452bad9fc98a76694a9f6e0b211bb2597e8f37
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-01-25 15:54:41 +0700

    change database uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017', { dbName: 'nest-api' }),
+    MongooseModule.forRoot('mongodb+srv://arizkymuhammad46:C9mlIhgwQUTH2lLa@efektenang.mbnssgs.mongodb.net', { dbName: 'nest-api' }),
     HeroModule,
   ],
   controllers: [AppController],
