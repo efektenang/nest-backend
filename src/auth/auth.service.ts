0e8e0df222d0e057f03328478a8521648cc0e8a6
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-01-29 16:16:36 +0700

    fix hashPassword for register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Auth } from './schemas/auth.schema';
 import { Model } from 'mongoose';
+import { hash } from 'bcryptjs'
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(Auth.name) private authModel: Model<Auth>) {}
   async create(createAuthDto: CreateAuthDto) {
-    const user = new this.authModel(createAuthDto)
-    return user.save()
+    const checkEmail = await this.authModel.find({
+      email: createAuthDto.email
+    }).select('-password')
+
+    if (checkEmail) throw new ConflictException('Email is already exist')
+    
+    const saveUser = await this.authModel.create({
+      ...createAuthDto,
+      password: await hash(createAuthDto.password, 10)
+    })
+    return saveUser
   }
 
-  async findAll(): Promise<Auth[]> {
-    const res = await this.authModel.find()
+  async findAll() {
+    const res = await this.authModel.find().select('-password')
     return res
   }
 
@@ -27,7 +37,7 @@ export class AuthService {
 
   async update(id: any, updateAuthDto: UpdateAuthDto) {
     const user = await this.authModel.findById(id)
-    if (!user) throw new Error("Update Failed! User not found.")
+    if (!user) throw new NotFoundException("Update Failed! User not found.")
 
     const updateUser = await this.authModel.updateOne({ _id: id }, { $set: updateAuthDto })
     return updateUser
